776120591505edd1c25638bf6badb211ed1b2818
trungson01 <traidatmaxmanh@gmail.com>
2025-06-22 19:10:36 +0700

    Save changes before adding backend subtree

--- backend/MyProject/MyAPI/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Users, Posts, Comments, Notifications
-
-# Register your models here.
-admin.site.register(Users)
-admin.site.register(Posts)
-admin.site.register(Comments)
-admin.site.register(Notifications)
-#     notification_id = models.AutoField(primary_key=True)
